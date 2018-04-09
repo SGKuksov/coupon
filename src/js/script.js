cbc46e782e1278efa5340b847df33fbcea2f587b
sgkuksov <sgkuksov1989@gmail.com>
2018-04-09 10:01:50 +0300

    Добавил скрипт открытия строки поиска на мобильном, исправил скрипт скрытия блока подписки

--- src/js/script.js
+++ src/js/script.js
@@ -4,12 +4,16 @@ $(document).ready(function(){
 
   //при нажатию на любую кнопку, имеющую класс .btn
   $("#callModalLocation").click(function() {
-    //открыть модальное окно с id="myModal"
-    $("#modalLocation").modal('show');
+    var doc_w = $(document).width();
+
+    if (doc_w >= 1024 ) {
+      //открыть модальное окно с id="myModal"
+      $("#modalLocation").modal('show');
+    }
   });
 
-  $(".subscribe .subscribe__close").click(function() {
-    // скрывает окно подписки
+  $(".subscribe__close").click(function() {
+    // скрытие блока подписки
     $(this).parent().hide();
   });
 
@@ -18,6 +22,30 @@ $(document).ready(function(){
     $(".filter__btn").removeClass("filter__btn--active");
     $(this).addClass("filter__btn--active");
   });
+
+  $(".page-header__search-icon").click(function() {
+    // показ строки поиска
+    $("form.page-header__search").addClass("page-header__search--show").parent().parent().css("padding", 0);
+    $(".page-header__input").attr("placeholder", "Поиск")
+    $(".page-header__cancel").addClass("page-header__cancel--show");
+    $(".page-header__submit").addClass("page-header__submit--show");
+  });
+
+  $(".page-header__cancel").click(function() {
+    // показ строки поиска
+    $("form.page-header__search").removeClass("page-header__search--show").parent().parent().css("padding", "8px");
+    $(".page-header__input").attr("placeholder", "")
+    $(".page-header__cancel").removeClass("page-header__cancel--show");
+    $(".page-header__submit").removeClass("page-header__submit--show");
+  });
+
+  // $(document).resize(function() {
+  //   // скрывает строку поиска при ресайзе
+  //   $("form.page-header__search").hide();
+  //   $(".page-header__cancel").hide();
+  //   $(".page-header__submit").removeClass("page-header__submit--show")
+  // });
+
 
 
 
